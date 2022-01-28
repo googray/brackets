cf5e796026c89be32effe170e7c17d1430d6d05d
googray <shat.rom07@gmail.com>
2022-01-28 14:34:28 +0100

    fix: update function

--- src/index.js
+++ src/index.js
@@ -1,35 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
+  let openBrackets = [];
+  let bracketsPair = {};
+  bracketsConfig.map((a) => openBrackets.push(a[0]));
+  bracketsConfig.forEach((a) => (bracketsPair[a[1]] = a[0]));
+
   let stack = [];
-  // let arr = bracketsConfig.flat();
   for (let i = 0; i < str.length; i++) {
-    if (
-      (str[i].length > 0 &&
-        str[i] === ")" &&
-        stack[stack.length - 1] === "(") ||
-      (str[i] === "]" && stack[stack.length - 1] === "[") ||
-      (str[i] === "}" && stack[stack.length - 1] === "{") ||
-      (str[i] === "|" && stack[stack.length - 1] === "|") ||
-      (str[i] === "1" && stack[stack.length - 1] === "2") ||
-      (str[i] === "3" && stack[stack.length - 1] === "4") ||
-      (str[i] === "5" && stack[stack.length - 1] === "6") ||
-      (str[i] === "7" && stack[stack.length - 1] === "7") ||
-      (str[i] === "8" && stack[stack.length - 1] === "8")
-    ) {
-      stack.pop();
-    } else if (
-      str[i] === "(" ||
-      str[i] === "[" ||
-      str[i] === "{" ||
-      str[i] === "|" ||
-      str[i] === "1" ||
-      str[i] === "3" ||
-      str[i] === "5" ||
-      str[i] === "7" ||
-      str[i] === "8"
-    ) {
-      stack.push(str[i]);
+    let current = str[i];
+    if (openBrackets.includes(current)) {
+      if (
+        Object.keys(bracketsPair).includes(current) &&
+        stack[stack.length - 1] === current
+      ) {
+        stack.pop();
+      } else {
+        stack.push(current);
+      }
     } else {
-      return false;
+      if (stack.length === 0) {
+        return false;
+      }
+      let topItem = stack[stack.length - 1];
+      if (bracketsPair[current] === topItem) {
+        stack.pop();
+      } else {
+        return false;
+      }
     }
   }
   return stack.length === 0;
